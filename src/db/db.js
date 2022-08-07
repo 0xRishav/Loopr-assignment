3ce72c95a67f2cb3391870e0e5018fa4859cb30a
Rishav Bharti <rishav.bharti@omnycomm.com>
2022-08-07 12:45:32 +0530

    add work order db

---
+++ src/db/db.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    document_name: "1332523453253",
+    upload_date: "2022-08-05T05:28:11.269Z",
+    upload_percentage: "100",
+  },
+  {
+    document_name: "8927340598203",
+    upload_date: "2022-08-05T05:28:11.269Z",
+    upload_percentage: "100",
+  },
+  {
+    document_name: "2345234647546",
+    upload_date: "2022-08-05T05:28:11.269Z",
+    upload_percentage: "50",
+  },
+  {
+    document_name: "8092347590234",
+    upload_date: "2022-08-05T05:28:11.269Z",
+    upload_percentage: "70",
+  },
+];
