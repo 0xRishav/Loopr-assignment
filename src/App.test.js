dc32c75d668f9b09f90f4617762b3e9a06644fe1
Rishav Bharti <rishav.bharti@omnycomm.com>
2022-08-07 12:50:41 +0530

    test: add api tests for fetching and adding workOrders

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {
+  addWorkOrderApi,
+  fetchWorkOrderDataApi,
+} from "./features/workOrder/WorkOrderApi";
+import WorkOrderData from "./db/db";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("fetchWorkOrder() using promises", () => {
+  it("should load work order data", async () => {
+    const res = await fetchWorkOrderDataApi();
+    expect(res).toBeDefined();
+    expect(res).toEqual(WorkOrderData);
+  });
+});
+
+describe("addWorkOrder() using promises", () => {
+  it("should add work order", async () => {
+    const res = await addWorkOrderApi();
+    expect(res).toBeDefined();
+    expect(res).toEqual([
+      ...WorkOrderData,
+      {
+        document_name: "13325234532532",
+        upload_date: "2022-08-05T05:28:11.269Z",
+        upload_percentage: "100",
+      },
+    ]);
+  });
 });
