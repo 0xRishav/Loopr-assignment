1c402d3ddddb726714a04f5b5ce086e23842dfcd
Rishav Bharti <rishav.bharti@omnycomm.com>
2022-08-07 12:46:17 +0530

    style: add theme to tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#3687D2",
+        black: "#23263B",
+        purple: "#444CA3",
+        white: "#F5F5F5",
+        grey: "#F2F2F2",
+        red: "#ff3333",
+        dark_green: "#5BC69F",
+        light_green: "#CEEEE2",
+      },
+    },
   },
   plugins: [],
 };
